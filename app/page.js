5bed4d6a213167d9201d11ab63bfb9bf1332751c
Batuhan Eskici <batuhan_eskici@hotmail.com>
2023-05-30 16:53:00 +0300

    add featured movie

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <main>Home</main>;
+import React from 'react';
+import HomeContainer from '@/containers/home';
+
+function HomePage() {
+  return <HomeContainer />;
 }
+
+export default HomePage;
---
+++ containers/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Movies from '@/mocks/movies.json';
+import { FeaturedMovie } from '@/components/featured-movie';
+
+function HomeContainer() {
+  return (
+    <div>
+      <FeaturedMovie movie={Movies.results[0]} />
+    </div>
+  );
+}
+
+export default HomeContainer;
