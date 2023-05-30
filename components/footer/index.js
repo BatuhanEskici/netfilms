b93e3e1f3eda02fd4be7e77669e97cf160e08538
Batuhan Eskici <batuhan_eskici@hotmail.com>
2023-05-30 16:29:52 +0300

    add footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
+
+import styles from './styles.module.css';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ by&nbsp;
+      <Link href="https://twitter.com/batuhann_e" target="_blank">
+        Batuhan Eskici
+      </Link>
+    </footer>
+  );
 }
 
 export default Footer;
